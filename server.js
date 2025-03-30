a6e4dea4f2700f4f01e6eb4d73981c3f601ca0dd
boxing <boxing@boxinghosting.dev>
2025-03-30 14:53:02 -0700

    Add calendar
    add calendar and update events

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const config = require('./config.json');
-const express = require('express');
-const utils = require('@un-boxing-hosting/boxing-hosting-utils');
-const db = undefined //new utils.db(config.db);
-const uuid = require('uuid').v4
-const session = require('express-session')
-const FileStore = require('session-file-store')(session);
-const passport = require('passport');
-const bcrypt = require('bcrypt');
+const config = require("./config.json");
+const express = require("express");
+const utils = require("@un-boxing-hosting/boxing-hosting-utils");
+const db = undefined; //new utils.db(config.db);
+const uuid = require("uuid").v4;
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const bcrypt = require("bcrypt");
 const os = require(`os`);
 const boxname = os.hostname();
 const port = 8101;
@@ -16,28 +16,28 @@ const saltRounds = 10;
 var dirname = "sites/PackLifecc/server";
 //const dirname = "server";
 if (boxname == "un-boxing-mans-pc") {
-    console.log(`Running on ${boxname}`)
-    dirname = "PackLifecc/server"
+	console.log(`Running on ${boxname}`);
+	dirname = "PackLifecc/server";
 }
 
-const logger = require('morgan');
-const favicon = require('serve-favicon');
+const logger = require("morgan");
+const favicon = require("serve-favicon");
 const app = express();
-app.use(logger('combined'));
+app.use(logger("combined"));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(
+	express.urlencoded({
+		extended: false,
+	})
+);
 app.set(`trust proxy`, true);
 
-app.use(favicon(`${dirname}/pix/favicon.ico`))
-app.use(express.static(dirname + "/"))
-app.use(`/events`, express.static(dirname + '/events.html'));
-app.use(`/about`, express.static(dirname + '/members.html'));
-app.use(`/members`, express.static(dirname + '/members.html'));
-
-
-
+app.use(favicon(`${dirname}/pix/favicon.ico`));
+app.use(express.static(dirname + "/"));
+app.use(`/events`, express.static(dirname + "/events.html"));
+app.use(`/about`, express.static(dirname + "/members.html"));
+app.use(`/members`, express.static(dirname + "/members.html"));
+app.use(`/calendar`, express.static(dirname + "/calendar.html"));
 
 // configure passport.js to use the local strategy
 /* passport.use(new LocalStrategy({
@@ -78,10 +78,9 @@ done(null, user);
 });
  */
 app.listen(port, () => {
-    console.log(`PackLifecc Listening on port ${port}`)
-
-})
+	console.log(`PackLifecc Listening on port ${port}`);
+});
 app.set("port", port);
 module.exports = {
-    app
-}
+	app,
+};
--- server/js/events.js
+++ server/js/events.js
@@ -1,86 +1,88 @@
 // JavaScript Document
 const host = "https://" + window.location.hostname; //"https://packlifecarclub.com";
 window.onload = async function () {
-    const events = await fetch(`${host}/json/events.json`);
-    const jsonEvents = await events.json();
-    console.log(jsonEvents);
-    var row = 0;
-    var pastrow = 0;
-    for (var i = 0; i < jsonEvents.list.length; i++) {
-        //every 3rd event create a new row
-            if (i % 3 == 0) {
-                row++;
-                var rowdiv = document.createElement("div");
-                rowdiv.className = "row";
-                rowdiv.id = "row" + row;
-                document.getElementById("container").appendChild(rowdiv);
-            }
-            await CreateBlock(jsonEvents.list[i], `row${row}`);
-    }
-    for (var i = 0; i < jsonEvents.pastlist.length; i++) {
-        //every 3rd event create a new row
-            if (i % 3 == 0) {
-                pastrow++;
-                var pastrowdiv = document.createElement("div");
-                pastrowdiv.className = "row";
-                pastrowdiv.id = "pastrow" + pastrow;
-                document.getElementById("past-container").appendChild(pastrowdiv);
-            }
-            await CreateBlock(jsonEvents.pastlist[i], `pastrow${pastrow}`);
-        
-
-    }
-}
+	const events = await fetch(`${host}/json/events.json`);
+	const jsonEvents = await events.json();
+	console.log(jsonEvents);
+	var row = 0;
+	var pastrow = 0;
+	for (var i = 0; i < jsonEvents.list.length; i++) {
+		//every 3rd event create a new row
+		if (i % 3 == 0) {
+			row++;
+			var rowdiv = document.createElement("div");
+			rowdiv.className = "row";
+			rowdiv.id = "row" + row;
+			document.getElementById("container").appendChild(rowdiv);
+		}
+		await CreateBlock(jsonEvents.list[i], `row${row}`);
+	}
+	for (var i = 0; i < jsonEvents.pastlist.length; i++) {
+		//every 3rd event create a new row
+		if (i % 3 == 0) {
+			pastrow++;
+			var pastrowdiv = document.createElement("div");
+			pastrowdiv.className = "row";
+			pastrowdiv.id = "pastrow" + pastrow;
+			document.getElementById("past-container").appendChild(pastrowdiv);
+		}
+		await CreateBlock(jsonEvents.pastlist[i], `pastrow${pastrow}`);
+	}
+};
 
 async function CreateBlock(info, row) {
-    console.log(info);
-/*     info ={
+	console.log(info);
+	/*     info ={
         type: "present",
         details: "Test",
         link: "https://www.facebook.com/Orchardscarsandcoffee",
         imageLink: "pix/orchards-cars-coffee.jpg",
     } */
-    var col = document.createElement("div");
-    col.className = "col-md-4";
-    var card = document.createElement("div");
-    if (info.type == "present") {
-        card.className = "panel panel-primary border border-5 border-success rounded-3";
-    }
-    if (info.type == "not-present") {
-        card.className = "panel panel-primary border border-5 border-primary rounded-3";
-    }
-    if (info.type == "past") {
-        card.className = "panel panel-primary border border-5 border-white rounded-3";
-    }
-    var cardHeader = document.createElement("div");
-    cardHeader.className = "panel-heading";
-    var a1 = document.createElement("a");
-    a1.href = info.link;
-    a1.target = "_blank";
-    a1.rel = "noopener noreferrer";
-    var h4 = document.createElement("h4");
-    var strong = document.createElement("strong");
-    strong.innerHTML = info.details;
-    var cardBody = document.createElement("div");
-    cardBody.className = "panel-body";
-    var a2 = document.createElement("a");
-    a2.href = info.imageLink;
-    if (info.imageLink != "#") {
-        var img = document.createElement("img");
-        img.src = info.imageLink;
-        img.className = "img-responsive";
-        img.alt = "Image";
-        img.style = "width:50%";
-        a2.appendChild(img);
-    }
+	var col = document.createElement("div");
+	col.className = "col-md-4";
+	var card = document.createElement("div");
+	if (info.type == "present") {
+		card.className =
+			"panel panel-primary border border-5 border-success rounded-3";
+	}
+	if (info.type == "not-present") {
+		card.className =
+			"panel panel-primary border border-5 border-primary rounded-3";
+	}
+	if (info.type == "past") {
+		card.className =
+			"panel panel-primary border border-5 border-white rounded-3";
+	}
+	var cardHeader = document.createElement("div");
+	cardHeader.className = "panel-heading";
+	var a1 = document.createElement("a");
+	a1.href = info.link;
+	a1.target = "_blank";
+	a1.rel = "noopener noreferrer";
+	var h4 = document.createElement("h4");
+	h4.id = `${info.id}`;
+	var strong = document.createElement("strong");
+	strong.innerHTML = info.details;
+	var cardBody = document.createElement("div");
+	cardBody.className = "panel-body";
+	var a2 = document.createElement("a");
+	a2.href = info.imageLink;
+	if (info.imageLink != "#") {
+		var img = document.createElement("img");
+		img.src = info.imageLink;
+		img.className = "img-responsive";
+		img.alt = "Image";
+		img.style = "width:50%";
+		a2.appendChild(img);
+	}
 
-    h4.appendChild(strong);
-    a1.appendChild(h4);
-    cardHeader.appendChild(a1);
-    card.appendChild(cardHeader);
-    cardBody.appendChild(a2);
-    card.appendChild(cardBody);
-    col.appendChild(card);
-    console.log(row);   
-    document.getElementById(row).appendChild(col);
+	h4.appendChild(strong);
+	a1.appendChild(h4);
+	cardHeader.appendChild(a1);
+	card.appendChild(cardHeader);
+	cardBody.appendChild(a2);
+	card.appendChild(cardBody);
+	col.appendChild(card);
+	console.log(row);
+	document.getElementById(row).appendChild(col);
 }
